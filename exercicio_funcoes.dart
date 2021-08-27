56172fae34d5d1c8b8ce31942d35000cb99b5797
c3iton <89215520+c3iton@users.noreply.github.com>
2021-08-27 15:24:09 -0300

    Add files via upload
    revisão - variáveis e estrutura de decisão

---
+++ exercicio_funcoes.dart
@@ -0,0 +1,62 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:funcoes/funcoes.dart' as funcoes;
+import 'package:test/expect.dart';
+
+//Devo chamar a funcao no void
+void main(List<String> arguments) {
+  //Chamando a funcao imprimir irá executar o que tem dentro da funcao imprimir
+  imprimir();
+  //Chamando a funcao com o parametro nome e atribuindo o nome Joao ao parametro
+  imprimirNome('Nome:');
+  //Chamando a funcao com rretorno e sem parametros
+//devolverOla()
+//O valor retornado na função devolverOla() abaixo, irá ser atribuido a variável mensagem
+  var mensagem = devolverOla();
+  print(mensagem);
+
+  //iRÁ RETORNAR O O RETORNO SETADO MAIS O PARAMETRO
+  var msg = devolverOlaNome('Estes são os produtos:');
+  print(msg);
+
+  print(devolverOla2());
+
+//Funcao Anonima
+  imprimirPessoa(() {
+    print('Bom dia');
+  });
+}
+//Funcao sem retorno e sem parametros
+
+void imprimir() {
+  print('Boa tarde. Este é o mercado São João');
+}
+
+//Funcao sem retorno e como parâmetros. O parametro é a variavel nome
+void imprimirNome(String nome) {
+  print("Digite seu nome");
+
+  nome = stdin.readLineSync();
+  print('Olá $nome aproveite nossas ofertas');
+}
+
+//Função com retorno e sem parametros
+String devolverOla() {
+  //Quando eu chamar irá retornar Ola, meus amigos por causa do return abaixo
+  return 'Temos alguns produtos em promoção no estoque, veja:';
+}
+
+//Funcao com retorno e com parâmetros
+String devolverOlaNome(String nome) {
+  return ' $nome Mamão, Abacate';
+}
+
+String devolverOla2() => 'Qual pruduto você gostaria de comprar?';
+
+//Funcao Anonima
+void imprimirPessoa(Function funcao) {
+  funcao();
+
+  print('Temos estes dois já mostrados');
+}
---
+++ exercicio_listas_estruturas.dart
@@ -0,0 +1,81 @@
+void main(List<String> arguments) {
+  //colecoes = listas, mapas, filas e conjuntos
+
+  //Lista de elementos do tipo String
+  //[] = Lista
+  //var nomes = <String>[]; = Declaracao de  Lista String
+  //Funcao que adicione clientes que realizaram compras manualmente,  enquanto houver produtos disponíveis
+  //Já estão pré cadastrados alguns nomes
+
+  print('Produtos. Códigos, Frutas, verduras e Clientes Cadastrados');
+  print('SysMarket');
+  var nomes = [
+    'Cliente Cadastrado 1',
+    'Cliente Cadastrado 2',
+    'Cliente Cadastrado 2',
+    'Cliente Cadastrado 3',
+    'Cliente Cadastrado4',
+    'Cliente Cadastrado'
+  ];
+
+//Estruturas de Repetição
+//While
+  var quantidadeElementos = nomes.length;
+  var contador = 0;
+  while (contador < quantidadeElementos) {
+    print(nomes[contador]);
+    contador++;
+  }
+
+  // Do While
+  //Cadastro dos códigos
+  var codigos = [
+    '9DJDSKS',
+    'KSDKHDSK',
+    'DKSKJHSDH',
+    'SHDKJS8S8',
+    'SHDJHSHK9',
+    'KHSDKHKJ5'
+  ];
+  var quantidadeElementos1 = codigos.length;
+
+  var contador1 = 0;
+
+  do {
+    print(codigos[contador1]);
+    contador1++;
+  } while (contador1 < quantidadeElementos1);
+
+  var tipoproduto = ['Arroz', 'Café', 'Pão', 'Feijão', 'Açucar', 'Sal'];
+  var quantidadeProdutos = tipoproduto.length;
+
+  for (var i = 0; i < quantidadeProdutos; i++) {
+    print(tipoproduto[i]);
+  }
+
+  //For In
+  //Este for percorre todo a String e imprime o resultado de cada posição
+
+  var frutas = ['Maçã', 'Mamão', 'Abacaxi', 'Kiwi', 'Melancia', 'Morango'];
+  for (var frutas in frutas) {
+    print(frutas);
+  }
+  //Função nomeada Imprimir
+  // void imprimir(String nome) {
+  // print(nome);
+  // }
+
+  //Função Anonima
+  var verdes = [
+    'Abacate',
+    'Tomate',
+    'Alface',
+    'Repolho',
+    'Batata',
+    'Batata Doce'
+  ];
+
+  verdes.forEach((verdes) => print(verdes));
+
+  //Este método percorre a lista nomes e chama a função imprimir pra imprimir cada um deles
+}
